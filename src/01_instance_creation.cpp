edbc582fc9c025ae5bcff391173f973ac10cea56
Rainzor <runze.wang02@outlook.com>
2024-01-02 23:47:40 +0800

    add 02 validation layers

--- src/01_instance_creation.cpp
+++ src/01_instance_creation.cpp
@@ -4,13 +4,14 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
-
+#include <vector>
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication {
 public:
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
@@ -19,6 +20,14 @@ public:
 private:
     GLFWwindow* window;//窗口句柄   
     VkInstance instance;//实例句柄
+
+    void initWindow() {
+        glfwInit();//初始化GLFW
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);//不使用OpenGL
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);//不可调整窗口大小
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);//创建窗口
+    }
 
     void initVulkan() {
         // The very first thing you need to do is
@@ -36,6 +45,42 @@ private:
         appInfo.pEngineName = "No Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.apiVersion = VK_API_VERSION_1_0;
+        
+        //A lot of information in Vulkan is passed 
+        //through structs instead of function parameters 
+        //它告诉Vulkan驱动程序需要使用的全局扩展和验证层
+        //全局是说它对整个程序有效，而不是特定于设备
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        //glfw extensions
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);//获取GLFW需要的扩展
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        //validation layers
+        createInfo.enabledLayerCount = 0;
+
+        //  create instance
+        //  param1: Pointer to struct with creation info
+        //  param2: Pointer to custom allocator callbacks
+        //  param3: Pointer to the variable that stores the handle to the new object
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+
+
+        //  check extensions
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);//获取可用扩展数量
+        std::vector<VkExtensionProperties> extensions(extensionCount);//创建扩展数组
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());//获取可用扩展列表
+        for(const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << std::endl;
+        }
     }
 
     void mainLoop() {
@@ -45,6 +90,7 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);//销毁vulkan实例
         glfwDestroyWindow(window);//销毁窗口
         glfwTerminate();//终止GLFW
     }
---
+++ src/02_validation_layers.cpp
@@ -0,0 +1,267 @@
+/*
+校验层常被用来做下面的工作：
+• 检测参数值是否合法
+• 追踪对象的创建和清除操作，发现资源泄漏问题
+• 追踪调用来自的线程，检测是否线程安全。
+• 将扁扐扉调用和调用的参数写入日志
+• 追调用进行分析和回放
+*/
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+//C++中的宏定义
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+//创建Debug instance
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+    //加载VkDebugUtilsMessengerEXT函数
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    } else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+//销毁Debug instance
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        func(instance, debugMessenger, pAllocator);
+    }
+}
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow* window;//窗口句柄   
+    VkInstance instance;//实例句柄
+    VkDebugUtilsMessengerEXT debugMessenger;//调试信息句柄
+
+    void initWindow() {
+        glfwInit();//初始化GLFW
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);//不使用OpenGL
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);//不可调整窗口大小
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);//创建窗口
+    }
+
+    void initVulkan() {
+        // The very first thing you need to do is
+        // initialize the Vulkan library by creating an instance
+        createInstance();
+
+        // setup debug messenger instance
+        setupDebugMessenger();
+
+    }
+    void createInstance() {
+
+        //  check validation layer support by LunarG
+        if (enableValidationLayers && !checkValidationLayerSupport()) {
+            throw std::runtime_error("validation layers requested, but not available!");
+        }
+
+        // to create an instance we'll first have to
+        // fill in a struct with some information about our application.
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        
+        //A lot of information in Vulkan is passed 
+        //through structs instead of function parameters 
+        //它告诉Vulkan驱动程序需要使用的全局扩展和验证层
+        //全局是说它对整个程序有效，而不是特定于设备
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        //glfw extensions
+        auto extensions = getRequiredExtensions();
+        createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+        createInfo.ppEnabledExtensionNames = extensions.data();
+
+        //validation layers
+        VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+        if (enableValidationLayers) {
+            // 在校验层启用的情况下，使用校验层
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());//启用的校验层数量
+            createInfo.ppEnabledLayerNames = validationLayers.data();//启用的校验层名称
+            // 获取调试信息
+            populateDebugMessengerCreateInfo(debugCreateInfo);
+            createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debugCreateInfo;
+        } else {
+            createInfo.enabledLayerCount = 0;//启用的校验层数量
+            createInfo.pNext = nullptr;
+        }
+
+        //  create instance
+        //  param1: Pointer to struct with creation info
+        //  param2: Pointer to custom allocator callbacks
+        //  param3: Pointer to the variable that stores the handle to the new object
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {//循环直到窗口关闭
+            glfwPollEvents();//处理事件:如果有事件触发，调用对应的回调函数
+        }
+    }
+
+    void cleanup() {
+        if(enableValidationLayers) {
+            DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);//销毁调试信息
+        }
+
+        vkDestroyInstance(instance, nullptr);//销毁vulkan实例
+        glfwDestroyWindow(window);//销毁窗口
+        glfwTerminate();//终止GLFW
+
+    }
+
+    // 配置调试信息
+    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
+        createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        //指定回调函数处理的消息级别
+        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        //指定回调函数处理的消息类型
+        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        //指定回调函数的指针
+        createInfo.pfnUserCallback = debugCallback;
+    }
+
+    void setupDebugMessenger() {
+        if (!enableValidationLayers) return;
+
+        VkDebugUtilsMessengerCreateInfoEXT createInfo;
+        populateDebugMessengerCreateInfo(createInfo);
+        //创建调试对象
+        if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
+            throw std::runtime_error("failed to set up debug messenger!");
+        }
+    }
+
+    // 获取扩展
+    std::vector<const char*> getRequiredExtensions() {
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);//获取GLFW需要的扩展
+
+        std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+        //根据是否启用校验层，返回所需的扩展列表
+        if (enableValidationLayers) {
+            extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);//获取调试信息，添加调试扩展
+        }
+
+        return extensions;
+    }
+
+    //请求所有可用的校验层
+    bool checkValidationLayerSupport() {
+        uint32_t layerCount;
+        //获取可用的校验层
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());//获取可用的校验层
+
+        //检查所有的校验层是否都被支持
+        for (const char* layerName : validationLayers) {
+            bool layerFound = false;
+
+            //遍历所有可用的校验层
+            for (const auto& layerProperties : availableLayers) {
+                //如果校验层被支持
+                if (strcmp(layerName, layerProperties.layerName) == 0) {
+                    layerFound = true;
+                    break;
+                }
+            }
+            //如果有校验层不被支持
+            if (!layerFound) {
+                return false;
+            }
+        }
+        return true;
+    }
+    // debug callback
+    // param: messageSeverity 消息级别：诊断、资源创建、警告、错误
+    // param: messageType 消息类型：与性能无关、违反规范的行为或者错误、影响vulkan性能的事件
+    // param: pCallbackData 指向包含消息的结构体: 包含
+    //        pMessage：包含调试信息的字符串
+    //        pObjects：与消息相关的Vulkan对象的句柄数组
+    //        objectCount：pObjects数组中的对象数量
+    // param: pUserData 指向回调函数的用户数据
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+        void* pUserData) 
+    {   
+        //example of messageSeverity
+        if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) {
+            // Message is important enough to show
+        }
+
+        //example of messageType
+        if (messageType & VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT) {
+            // Message is related to validation or descriptive debug info
+        }
+
+        //输出调试信息
+        std::cerr << "validation layer: " 
+                << pCallbackData->pMessage 
+                << std::endl;
+                
+        //返回值表示引发校验层处理的API调用是否被中断
+        //如果返回VK_TRUE，表示应用程序应该终止调用引发此回调的Vulkan函数
+        //如果返回VK_FALSE，表示应用程序应该继续进行调用引发此回调的Vulkan函数
+        return VK_FALSE;
+    }
+
+
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
